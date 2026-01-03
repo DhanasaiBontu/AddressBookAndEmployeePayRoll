ffed855264780c7d54d7cf536a021ebfc19f1fee
DhanasaiBontu <bdhanasaichowdary@gmail.com>
2026-01-03 23:39:04 +0530

    UC1: Create contact with person details

---
+++ AddressBook/src/AddressBookMain.java
@@ -0,0 +1,18 @@
+public class AddressBookMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address Book Program");
+
+        Contact person = new Contact(
+                "John",
+                "Doe",
+                "MG Road",
+                "Bangalore",
+                "Karnataka",
+                "560001",
+                "9876543210",
+                "john.doe@gmail.com"
+        );
+
+        person.displayContact();
+    }
+}
