325fdcf4d379d547a0b27a6ecb1b0c88fce2678e
DhanasaiBontu <bdhanasaichowdary@gmail.com>
2026-01-04 00:19:22 +0530

    UC6: Calculate wages till total working hours or days reached

--- EmployeePayRoll/src/EmployeePayrollMain.java
+++ EmployeePayRoll/src/EmployeePayrollMain.java
@@ -7,35 +7,42 @@ public class EmployeePayrollMain {
         int IS_FULL_TIME = 1;
         int IS_PART_TIME = 2;
         int EMP_RATE_PER_HOUR = 20;
-        int NUM_OF_WORKING_DAYS = 20;
+        int MAX_WORKING_DAYS = 20;
+        int MAX_WORKING_HOURS = 100;
 
-        int empHrs = 0;
-        int totalEmpWage = 0;
+        int totalEmpHrs = 0;
+        int totalWorkingDays = 0;
 
-        for (int day = 1; day <= NUM_OF_WORKING_DAYS; day++) {
+        while (totalEmpHrs < MAX_WORKING_HOURS && totalWorkingDays < MAX_WORKING_DAYS) {
+
+            totalWorkingDays++;
 
             int empCheck = (int) Math.floor(Math.random() * 3);
+            int empHrs = 0;
 
             switch (empCheck) {
                 case IS_FULL_TIME:
                     empHrs = 8;
-                    System.out.println("Day " + day + ": Employee is Full Time");
+                    System.out.println("Day " + totalWorkingDays + ": Employee is Full Time");
                     break;
 
                 case IS_PART_TIME:
                     empHrs = 4;
-                    System.out.println("Day " + day + ": Employee is Part Time");
+                    System.out.println("Day " + totalWorkingDays + ": Employee is Part Time");
                     break;
 
                 default:
                     empHrs = 0;
-                    System.out.println("Day " + day + ": Employee is Absent");
+                    System.out.println("Day " + totalWorkingDays + ": Employee is Absent");
             }
 
-            int dailyWage = empHrs * EMP_RATE_PER_HOUR;
-            totalEmpWage += dailyWage;
+            totalEmpHrs += empHrs;
         }
 
-        System.out.println("Total Employee Wage for Month: " + totalEmpWage);
+        int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+
+        System.out.println("Total Working Days: " + totalWorkingDays);
+        System.out.println("Total Working Hours: " + totalEmpHrs);
+        System.out.println("Total Employee Wage: " + totalEmpWage);
     }
 }
