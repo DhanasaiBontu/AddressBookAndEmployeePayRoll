7fb8a351766ca41a6d3890bcb52d54bbb666fea6
DhanasaiBontu <bdhanasaichowdary@gmail.com>
2026-01-04 00:09:37 +0530

    UC3: Add part time employee and wage

--- EmployeePayRoll/src/EmployeePayrollMain.java
+++ EmployeePayRoll/src/EmployeePayrollMain.java
@@ -1,23 +1,30 @@
 public class EmployeePayrollMain {
 
     public static void main(String[] args) {
+
         System.out.println("Welcome to Employee Wage Computation Program");
 
-        int IS_PRESENT = 1;
+        int IS_FULL_TIME = 1;
+        int IS_PART_TIME = 2;
         int EMP_RATE_PER_HOUR = 20;
-        int FULL_DAY_HOURS = 8;
 
-        int empCheck = (int) Math.floor(Math.random() * 2);
+        int empHrs = 0;
         int empWage = 0;
 
-        if (empCheck == IS_PRESENT) {
-            System.out.println("Employee is Present");
-            empWage = EMP_RATE_PER_HOUR * FULL_DAY_HOURS;
+        int empCheck = (int) Math.floor(Math.random() * 3);
+
+        if (empCheck == IS_FULL_TIME) {
+            empHrs = 8;
+            System.out.println("Employee is Full Time");
+        } else if (empCheck == IS_PART_TIME) {
+            empHrs = 4;
+            System.out.println("Employee is Part Time");
         } else {
+            empHrs = 0;
             System.out.println("Employee is Absent");
-            empWage = 0;
         }
 
-        System.out.println("Daily Employee Wage: " + empWage);
+        empWage = empHrs * EMP_RATE_PER_HOUR;
+        System.out.println("Employee Wage: " + empWage);
     }
 }
